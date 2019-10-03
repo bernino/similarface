b2dc9958fc70ecc9bc63141162a9ead1322b6146
Bernino Lind <bernino@gmail.com>
2019-10-03 14:12:30 +0200

    reiterated

--- similarface.py
+++ similarface.py
@@ -5,7 +5,6 @@ import pickle
 import os
 import glob
 from imutils import build_montages
-#from imageai.Detection import ObjectDetection
 import keras
 from keras.preprocessing import image
 from keras.models import load_model
@@ -27,18 +26,6 @@ emotion_model_path = '_mini_XCEPTION.102-0.66.hdf5'
 emotion_classifier = load_model(emotion_model_path, compile=False)
 emotions = ["angry" ,"disgust","scared", "happy", "sad", "surprised",
  "neutral"]
-
-#my_path = os.getcwd()
-#detector = ObjectDetection()
-#detector.setModelTypeAsRetinaNet()
-#detector.setModelPath( os.path.join(my_path , "resnet50_coco_best_v2.0.1.h5"))
-#detector.loadModel()
-
-#font                   = cv2.FONT_HERSHEY_SIMPLEX
-#bottomLeftCornerOfText = (10,100)
-#fontScale              = 0.5
-#fontColor              = (255,255,255)
-#lineType               = 1
 
 def sql_connection():
     try:
@@ -89,7 +76,6 @@ def pickledump(subject):
 imgs = get_imgs()
 for i, img in enumerate(imgs):
     data = get_encoding(img, cur)
-    #objectsfile = str(img)+"-objects.jpg"
     if not data:
         print("encoding "+ str(img))
         current_image = face_recognition.load_image_file(img)
@@ -97,14 +83,10 @@ for i, img in enumerate(imgs):
         face_encodings = face_recognition.face_encodings(
                          current_image, 
                          face_locations)
+
         image = cv2.imread(img)
         bbox, label, conf = cv.detect_common_objects(image)
-        #detections = detector.detectObjectsFromImage(input_image=img, output_image_path=os.path.join(my_path , objectsfile))
         detections = dict(label=label, conf=conf, bbox=bbox)
-        #for m, things in enumerate(detections):
-        #    # this overwrites detections and adds the array as a list
-        #    listed = things['box_points'].tolist()
-        #    things['box_points']=listed
         objects_detected_json = json.dumps(detections)
 
         for face_location, face_encoding in zip(face_locations, face_encodings):
@@ -121,17 +103,7 @@ for i, img in enumerate(imgs):
             face_imagetest = img_to_array(face_imagetest)
             face_imagetest = np.expand_dims(face_imagetest, axis=0)
             predicted_emotions = emotion_classifier.predict(face_imagetest)[0]
-            #max_emotion_probability = np.max(predicted_emotions)
-            #max_emotion = emotions[predicted_emotions.argmax()]
             predicted_emotions = predicted_emotions.tolist()
-            #emotionstring = ""
-            #for j, v in enumerate(predicted_emotions):
-            #    emotion = emotions[j]
-            #    emotionstring += str(emotion) + " - p:"+ str(v) + "\n"
-            #emotionstring += "mostly " + str(max_emotion) + " with prob: " + str(max_emotion_probability)
-            #print(emotionstring)
-            #cv2.imshow(img,face_image)
-            #cv2.waitKey(0)
 
             # pickling all the variables 
             face_image_pickled = pickledump(face_image)
@@ -174,8 +146,6 @@ for k, img2 in enumerate(imgs_to_match):
     for face_location2, face_encoding2 in zip(face_locations2, face_encodings2):
         print("matching face {}".format(img2))
         faces = []
-        #original_face = known_face_metadata[j]["image"]
-        #original_face_pic = known_face_metadata[j]["face"]
         top, right, bottom, left = face_location2
         face_image2 = current_image2[top:bottom, left:right]
         face_image2 = cv2.cvtColor(face_image2, cv2.COLOR_BGR2RGB)
@@ -184,8 +154,8 @@ for k, img2 in enumerate(imgs_to_match):
         for f, faces_encoded in enumerate(known_face_encodings):
             face_distances = face_recognition.face_distance([faces_encoded], face_encoding2)
             for i, face_distance in enumerate(face_distances): 
-                matching_face = known_face_metadata[f]["image"] 
                 if face_distance < 0.62:
+                    matching_face = known_face_metadata[f]["image"] 
                     print("Added {} to {} with distance {}".format(matching_face, img2, face_distance))
                     image = known_face_metadata[f]["face"]
                     faces.append(image)
